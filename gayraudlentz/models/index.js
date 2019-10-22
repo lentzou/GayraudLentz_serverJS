16b39ce38773e6cf7c6ecfedf37fd9d1b246caff
flo <flo>
2019-10-22 13:14:15 +0200

    Socket and chat

--- gayraudlentz/models/index.js
+++ gayraudlentz/models/index.js
@@ -34,29 +34,22 @@ const
     io = require("socket.io"),
     server = io.listen(8000);
 
-let
-    sequenceNumberByClient = new Map();
-
-// event fired every time a new client connects:
-server.on("connection", (socket) => {
+server.on("connection", (socket) => {  
   console.info(`Client connected [id=${socket.id}]`);
-  // initialize this client's sequence number
-  sequenceNumberByClient.set(socket, 1);
 
-  // when socket disconnects, remove it from the list:
-  socket.on("disconnect", () => {
-      sequenceNumberByClient.delete(socket);
+  socket.emit('newConnection', 'Vous êtes bien connecté !');
+  socket.broadcast.emit('newConnection', 'Un autre client vient de se connecter !');
+
+	socket.on('generalMessage', function (message) {
+    console.info('Un client me parle ! Il me dit : ' + message);
+    socket.broadcast.emit('generalMessage', message);
+  });	
+    
+  socket.on("disconnect", () => {      
       console.info(`Client gone [id=${socket.id}]`);
   });
 });
 
-// sends each client its current sequence number
-setInterval(() => {
-  for (const [client, sequenceNumber] of sequenceNumberByClient.entries()) {
-      client.emit("seq-num", sequenceNumber);
-      sequenceNumberByClient.set(client, sequenceNumber + 1);
-  }
-}, 1000);
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
