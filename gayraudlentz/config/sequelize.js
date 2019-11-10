5be9149abc97d1e46995cbcda779c5c749d03c98
FrancoisGayraud <francois.gayraud@epitech.eu>
2019-11-10 20:14:20 +0100

    env

--- gayraudlentz/config/sequelize.js
+++ gayraudlentz/config/sequelize.js
@@ -1,9 +1,18 @@
 const Sequelize = require('sequelize');
+let db;
+
+if (process.env.ENV_NODE ===  "prod") {
+    db = 'database_prod'
+} else if (process.env.ENV_TEST === "test") {
+    db = 'database_test'
+} else {
+    db = 'database_development'
+}
 
 const sequelize = new Sequelize(
-  'database_development', 'root', '', {
-    host: 'localhost',
-    dialect: 'mysql',
+    db, 'root', '', {
+        host: 'localhost',
+        dialect: 'mysql',
   });
 
 module.exports =  sequelize;
